9df5b142d29d9d11aa764d281da18adfb71d0b47
tokdok <51758635+tokdok@users.noreply.github.com>
2019-06-12 20:10:59 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir $tmpfoldername
-git clone https://github.com/MIksterww3/test.git /tmp/$tmpfoldername
+git clone https://github.com/tokdok/default.git /tmp/$tmpfoldername
 cd /tmp/$tmpfoldername
 mv /tmp/$tmpfoldername/defaultsoftwarename /tmp/$tmpfoldername/$softwarename
 chmod +x /tmp/$tmpfoldername/$softwarename
